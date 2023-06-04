16e1e6458a4471815e8318f38e1ca993d41656bb
hemashreesankappa <hemashree96@gmail.com>
2023-06-04 12:34:43 -0400

    media query changes

--- js/script.js
+++ js/script.js
@@ -25,3 +25,13 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+///////////////////////////////////////////////////////////
+// Make Mobile nav work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
